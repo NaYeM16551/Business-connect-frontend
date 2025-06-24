7d1b60cbdae3f38820cb1dc99f4ca1e2a6d46ca9
M.M.Nayem <mmnayem25@gmail.com>
2025-06-25 04:05:05 +0600

    urls updated

--- src/pages/VerifyEmail.tsx
+++ src/pages/VerifyEmail.tsx
@@ -36,7 +36,7 @@ const VerifyEmail = () => {
     (async (t: string) => {
       try {
         const response = await fetch(
-          `http://localhost:8080/api/v1/auth/verify-email?token=${t}`,
+          `http://57.159.26.157:8080/api/v1/auth/verify-email?token=${t}`,
           {
             method: "GET",
             headers: { "Content-Type": "application/json" },
